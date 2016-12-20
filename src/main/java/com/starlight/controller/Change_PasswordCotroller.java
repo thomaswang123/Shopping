f282acdf094728f3509436517a3505b6e7bff24c
“ywanghuayu <thomas.wang@starlight-sms.com>
2016-12-20 18:50:53 +0800

    通过密保进行修改用户密码以及一些保密性的修改

---
+++ src/main/java/com/starlight/controller/Change_PasswordCotroller.java
@@ -0,0 +1,25 @@
+package com.starlight.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by thomas.wang on 2016/12/20.
+ */
+@Controller
+public class Change_PasswordCotroller {
+
+
+    //进行判断密保问题域答案是否一致，进行修改密码等
+    @RequestMapping("alert.do")
+    public String consistency_Of_Judgment(){
+
+    }
+
+   //修改密码
+
+    //修改密保以及密保问题
+
+    //修改支付密码
+}
---
+++ src/main/java/com/starlight/dao/Change_PasswordDao.java
@@ -0,0 +1,18 @@
+package com.starlight.dao;
+
+import com.starlight.entity.PasswordProtecTion;
+
+/**
+ * Created by thomas.wang on 2016/12/20.
+ */
+public interface Change_PasswordDao {
+
+    /**
+     * 通过id查询密保与密保问题进行判断是否一致
+     * @param u_id
+     * @return
+     */
+    public List<PasswordProtecTion> findALLById(int u_id);
+
+    public int alertAllById(int u_id);
+}
