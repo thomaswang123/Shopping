7d6f455be0a7e576e779ca86f605a6c2026ec822
“ywanghuayu <thomas.wang@starlight-sms.com>
2016-12-20 12:45:02 +0800

    配置已经搭配完好

--- src/main/java/com/starlight/controller/UserController.java
+++ src/main/java/com/starlight/controller/UserController.java
@@ -3,6 +3,7 @@ package com.starlight.controller;
 import com.starlight.dao.UserDao;
 import com.starlight.entity.User;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,29 +15,17 @@ import java.util.List;
  */
 @Controller
 public class UserController {
-/*    @Resource
-	private UserDao userDao;
-
-	public UserDao getUserDao() {
-		return userDao;
-	}
-
-	public void setUserDao(UserDao userDao) {
-		this.userDao = userDao;
-	}*/
-
-@RequestMapping("login.do")
-   public String test(){
-		ApplicationContext act =
-				/*new ClassPathXmlApplicationContext("*//*spring-mvc.xml");*/
-		        new FileSystemXmlApplicationContext("");
-		System.out.println(act);
-		UserDao userdao = act.getBean(UserDao.class);
-		System.out.println("进来了");
-		List<User> list = userdao.findAll();
-		for (User lists : list) {
-			System.out.println(lists.getU_id() + "," + lists.getU_name() + "," + lists.getU_pawd());
-		}
-		return "index";
-	}
+    @RequestMapping("login.do")
+    public String test(String username, String password) {
+        ApplicationContext act =
+                new ClassPathXmlApplicationContext("spring/spring-mvc.xml");
+        System.out.println(act);
+        UserDao userdao = act.getBean(UserDao.class);
+        System.out.println("进来了");
+        List<User> list = userdao.findAll();
+        for (User lists : list) {
+            System.out.println(lists.getU_id() + "," + lists.getU_name() + "," + lists.getU_pwd());
+        }
+        return "Index";
+    }
 }
