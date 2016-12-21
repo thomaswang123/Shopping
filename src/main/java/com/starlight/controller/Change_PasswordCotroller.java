5c3227ec2b53e907bc68eba34418556edc16575f
“ywanghuayu <thomas.wang@starlight-sms.com>
2016-12-21 18:31:29 +0800

    通过密保进行修改用户密码以及一些保密性的修改

--- src/main/java/com/starlight/controller/Change_PasswordCotroller.java
+++ src/main/java/com/starlight/controller/Change_PasswordCotroller.java
@@ -1,25 +1,21 @@
 package com.starlight.controller;
 
-import com.starlight.entity.PasswordProtecTion;
 import com.starlight.entity.User;
 import com.starlight.entity.Wallet;
-import com.starlight.service.Change_PasswordService;
 import com.starlight.serviceimp.Change_PasswordServiceImp;
 import com.starlight.serviceimp.UserServiceImp;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * Created by thomas.wang on 2016/12/20.
  */
 @Controller
 public class Change_PasswordCotroller {
-   //用来记录改用户id方便其他操作
+    //用来记录改用户id方便其他操作
     private int u_id = 0;
     //获取对用户业务进行处理的对象
     @Resource
@@ -49,9 +45,10 @@ public class Change_PasswordCotroller {
     @RequestMapping("answer.do")
     @ResponseBody
     public String consistency_OfJudgment(String[] result) {
+        System.out.println("aaaa");
         //进行判断答案是否正确
         // 返回的是ajax的数据，不是页面的名字
-        return  change_PasswordServiceImp.Answer_Comparison(u_id,result);
+        return change_PasswordServiceImp.Answer_Comparison(u_id, result);
     }
 
 
@@ -63,7 +60,7 @@ public class Change_PasswordCotroller {
         //用户账号对应的id
         if ((u_id = userServiceImp.byname_GainId(username)) != 0) {
             //这里反回值是ajax的回调函数中数据！不是页面
-            return change_PasswordServiceImp.findQuestionById(u_id);
+            return change_PasswordServiceImp.findQuestionAndIddByUid(u_id);
         }
         return null;
     }
@@ -72,11 +69,24 @@ public class Change_PasswordCotroller {
     @RequestMapping("alertAll.do")
     //此注解不能省略 否则ajax无法接受返回值
     @ResponseBody
-    public String  alertAll(User user, String[] result, Wallet wallet,String password,String payment_code){
-          System.out.println(payment_code);
-          System.out.println(password);
-          System.out.println(result);
-
-          return "aa";
+    public String alertAll(User user, String[] result, Wallet wallet, String password, String payment_code) {
+        //用户的密码
+        user.setU_id(u_id);
+        user.setU_pwd(password);
+        //用户的支付密码
+       wallet.setW_id(u_id);
+       wallet.setW_pwd(Integer.parseInt(payment_code));
+ /*      System.out.println(ppid);*/
+       //进行处理并返回结果
+     /*   return  change_PasswordServiceImp.alertAllById(user, wallet, result);*/
+        return "Aaa";
     }
+
+    @RequestMapping("alertPpAll.do")
+    @ResponseBody
+    public String alertPpAll(String[] result,User user, String password, String payment_code,String[] ppid){
+        System.out.println(result);
+        return  "Aaa";
+    }
+
 }
