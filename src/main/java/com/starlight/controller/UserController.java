8f64b20d86b58cd4081f42ea2ce4ff52c2935cdd
“ywanghuayu <thomas.wang@starlight-sms.com>
2016-12-22 13:31:41 +0800

    pull

--- src/main/java/com/starlight/controller/UserController.java
+++ src/main/java/com/starlight/controller/UserController.java
@@ -28,30 +28,103 @@ public class UserController {
     @Resource
     private AminServiceImp aminServiceImp;
 
-    public AminServiceImp getAminServiceImp() {
-        return aminServiceImp;
+@Resource
+IUser iUser;
+@Resource
+IUserInfo iUserInfo;
+@Resource
+IPassWordProtection iPassWordProtection;
+@Resource
+IWallet iWallet;
+
+/**
+ * 用户注册方法
+ * @param httpServletRequest
+ * @param httpServletResponse
+ *
+ */
+@RequestMapping("/register.do")
+public String  register(String nickName,String account_number,String tel,String age,String sex,String password,
+                        String w_paypwd,
+                        HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse){
+    
+    System.out.println("进入注册");
+    
+    String address=httpServletRequest.getParameter("region")+httpServletRequest.getParameter("province")
+                           +httpServletRequest.getParameter("city")+httpServletRequest.getParameter("area");
+    
+    System.out.println(address);
+//		插入用户表数据
+    User user=new User();
+    user.setAccount(account_number);
+    user.setPassword(password);
+    iUser.register(user);
+//		获取用户id
+    int u_id=iUser.findIdByUser(account_number);
+//		插入用户详细信息表数据
+    UserInfo userInfo=new UserInfo();
+    userInfo.setId(u_id);
+    userInfo.setNickname(nickName);
+    userInfo.setAge(Integer.parseInt(age));
+    userInfo.setAddress(address);
+    userInfo.setPhone(tel);
+    userInfo.setSex(sex);
+    
+    iUserInfo.register(userInfo);
+//		插入密保表数据
+    String question1= httpServletRequest.getParameter("question1");
+    String question2= httpServletRequest.getParameter("question2");
+    String question3= httpServletRequest.getParameter("question3");
+    String answer1= httpServletRequest.getParameter("answer1");
+    String answer2= httpServletRequest.getParameter("answer2");
+    String answer3= httpServletRequest.getParameter("answer3");
+    String [] question={question1,question2,question3};
+    String [] answer={answer1,answer2,answer3};
+    
+    for(int i=0;i<question.length;i++){
+//			判断是否为空
+        if(question[i]!=null && answer[i]!=null){
+            PassWordProtection passWordProtection=new PassWordProtection();
+            passWordProtection.setUserId(u_id);
+            passWordProtection.setQuestion(question[i]);
+            passWordProtection.setAnswer(answer[i]);
+//				插入数据
+            iPassWordProtection.addPWP(passWordProtection);
+        }
     }
 
-    public void setAminServiceImp(AminServiceImp aminServiceImp) {
-        this.aminServiceImp = aminServiceImp;
-    }
+//		插入钱包表数据
+    Wallet wallet=new Wallet();
+    wallet.setId(u_id);
+    wallet.setPassword(Integer.parseInt(w_paypwd));
+    iWallet.register(wallet);
+    
+    return "redirect:index.html";
+    
+}
 
-    public UserinfoServiceImp getUserinfoServiceImp() {
-        return userinfoServiceImp;
+/**
+ * 匹配账号方法
+ * @param name
+ * @param httpServletRequest
+ * @param httpServletResponse
+ * @return
+ */
+@RequestMapping(value = "/checkAccout.do",produces = "text/html;charset=UTF-8")
+@ResponseBody
+public String checkAccout(String name, HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) {
+    System.out.println("进入账号验证");
+//		匹配输入的账号是否符合规则并查询是否存在该账号
+    
+    if (iUser.findAccount(name).isEmpty()) {
+        System.out.println("进入账号正确:" + "<label style='color:green'>√</label>");
+        return "true";
+    }else{
+        System.out.println("进入账号正确:"  + "<label style='color:green'>√</label>");
+        return "false";
     }
-
-    public void setUserinfoServiceImp(UserinfoServiceImp userinfoServiceImp) {
-        this.userinfoServiceImp = userinfoServiceImp;
-    }
-
-    public UserServiceImp getUserServiceImp() {
-        return userServiceImp;
-    }
-
-    public void setUserServiceImp(UserServiceImp userServiceImp) {
-        this.userServiceImp = userServiceImp;
-    }
-
+    
+    
     //登陆验证
     @RequestMapping("login.do")
     public String test(String username, String password,
